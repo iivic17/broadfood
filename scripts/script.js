746f5a9e3247955ac545d7c2d9e28d6b766ff9b6
iivic17 <ilijaivic17@gmail.com>
2021-12-05 19:35:38 +0100

    Implementing smooth scrolling

--- scripts/script.js
+++ scripts/script.js
@@ -9,6 +9,36 @@ const btnNavEl = document.querySelector('.btn-mobile-nav');
 const headerEl = document.querySelector('.header');
 
 btnNavEl.addEventListener('click', () => {
+	toggleMobileNavigation();
+});
+
+const toggleMobileNavigation = () => {
 	headerEl.classList.toggle('nav-open');
 	htmlEl.classList.toggle('overflow-hidden');
+};
+
+// Smooth scrolling animation
+const allLinks = document.querySelectorAll('a:link');
+
+allLinks.forEach((el) => {
+	el.addEventListener('click', (e) => {
+		e.preventDefault();
+		const href = el.getAttribute('href');
+
+		if (href === '#') {
+			window.scrollTo({
+				top: 0,
+				behavior: 'smooth',
+			});
+		} else if (href.startsWith('#')) {
+			const sectionEl = document.querySelector(href);
+			sectionEl.scrollIntoView({
+				behavior: 'smooth',
+			});
+		}
+
+		if (el.classList.contains('main-nav-link')) {
+			toggleMobileNavigation();
+		}
+	});
 });
